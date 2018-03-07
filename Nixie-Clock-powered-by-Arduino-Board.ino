0037328d97e92f9c1a0f68e9a6928e0ad01ecc9f
Konrad Witkowski <konradwitkowski01@gmail.com>
2018-03-07 19:38:39 +0100

    Add init of 74HC595 shift register. Add write data to shift registers, add set function to set nixie (binary driver input), add nixie binary tab to store number in binary with LOW/HIGH state for shift registers

--- Nixie-Clock-powered-by-Arduino-Board.ino
+++ Nixie-Clock-powered-by-Arduino-Board.ino
@@ -7,6 +7,18 @@
 
 #include "DS3231.h"
 
+int SER_Pin = 11;   //pin 14 on the 75HC595 (Data Pin connected to DS of 74HC595)
+int RCLK_Pin = 8;  //pin 12 on the 75HC595 (Latch Pin connected to ST_CP of 74HC595)
+int SRCLK_Pin = 12; //pin 11 on the 75HC595 (Clock Pin connected to SH_CP of 74HC595)
+
+//How many of the shift registers
+#define number_of_74hc595s 3
+
+//The miltiple by 8 is static, do not touch
+#define numOfRegisterPins number_of_74hc595s * 8
+
+boolean registers[numOfRegisterPins];
+
 // Init the DS3231 using the hardware interface
 DS3231  rtc(SDA, SCL);
 
@@ -14,6 +26,15 @@ void setup()
 {
   // Setup Serial connection
   Serial.begin(115200);
+
+  // Initialize 74HC595 pin's
+  pinMode(SER_Pin, OUTPUT);
+  pinMode(RCLK_Pin, OUTPUT);
+  pinMode(SRCLK_Pin, OUTPUT);
+
+  //reset all register pins
+  clearRegisters();
+  writeRegisters();
 
   // Initialize the rtc object
   rtc.begin();
@@ -40,3 +61,53 @@ void loop()
   // Wait one second before repeating :)
   delay (1000);
 }
+
+
+//set all register pins to LOW
+void clearRegisters() {
+  for (int i = numOfRegisterPins - 1; i >=  0; i--) {
+    registers[i] = LOW;
+  }
+  writeRegisters();
+}
+
+//Set and display/write registers
+void writeRegisters() {
+  digitalWrite(RCLK_Pin, LOW);
+  for (int i = numOfRegisterPins - 1; i >=  0; i--) {
+    digitalWrite(SRCLK_Pin, LOW);
+    int val = registers[i];
+    digitalWrite(SER_Pin, val);
+    digitalWrite(SRCLK_Pin, HIGH);
+  }
+  digitalWrite(RCLK_Pin, HIGH);
+}
+
+//set an individual pin HIGH or LOW
+void setRegisterPin(int index, int value) {
+  registers[index] = value;
+}
+
+int numberNixieBinTab[10][4] = {
+  {LOW, LOW, LOW, LOW}, // 0  {0,0,0,0},
+  {LOW, LOW, LOW, HIGH}, // 1  {0,0,0,1},
+  {LOW, LOW, HIGH, LOW}, // 2  {0,0,1,0},
+  {LOW, LOW, HIGH, HIGH}, // 3  {0,0,1,1},
+  {LOW, HIGH, LOW, LOW}, // 4  {0,1,0,0},
+  {LOW, HIGH, LOW, HIGH}, // 5  {0,1,0,1),
+  {LOW, HIGH, HIGH, LOW}, // 6  {0,1,1,0},
+  {LOW, HIGH, HIGH, HIGH}, // 7  {0,1,1,1},
+  {HIGH, LOW, LOW, LOW}, // 8  {1,0,0,0},
+  {HIGH, LOW, LOW, HIGH} // 9  {1,0,0,1}
+};
+
+void setNixie(int nixieIndex, int number) {
+  int nixieRegistersShift = 4 * nixieIndex;
+  setRegisterPin(0 + nixieRegistersShift, numberNixieBinTab[number][0]);
+  setRegisterPin(1 + nixieRegistersShift, numberNixieBinTab[number][1]);
+  setRegisterPin(2 + nixieRegistersShift, numberNixieBinTab[number][2]);
+  setRegisterPin(3 + nixieRegistersShift, numberNixieBinTab[number][3]);
+  writeRegisters();
+}
+
+
